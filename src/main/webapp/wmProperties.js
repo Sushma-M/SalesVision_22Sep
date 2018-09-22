64f96195b1c8c9c9273a8dc2bbb221469627285a
Naga Sushma Maddirala <sushma.maddirala@wavemaker.com>
2018-09-22 04:18:44 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "clear-glass",
+  "defaultLanguage" : "en",
+  "displayName" : "SalesVision_22Sep",
+  "homePage" : "Main",
+  "name" : "SalesVision_22Sep",
+  "platformType" : "WEB",
+  "securityEnabled" : "true",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0",
+  "xsrf_header_name" : "X-WM-XSRF-TOKEN"
+};
